e20301bb6c83b698a272d3995d5fb7dfaa736534
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-11-05 15:08:33 +0500

    apple

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import MainLayout from './Layouts/MainLayout'
+import MainLayout from './layouts/MainLayout';
 import { Route, Routes } from 'react-router-dom'
 import Likes from "./pages/Likes"
 import Home from "./pages/Home"
